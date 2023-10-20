a24ba4a8d66ab9b9f640358a2114de2946826355
Tecrak <87039557+Tecrak@users.noreply.github.com>
2023-10-20 11:02:36 +0100

    Update add.py

--- add.py
+++ add.py
@@ -1,1 +1,12 @@
+$servername = "localhost";
+$username = "root";
+$password = "28080303sTss$!M";
+$database = "form_data";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
