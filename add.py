a862374fe67e0274e8ebd335c9dc4966d73e2dc7
Tecrak <87039557+Tecrak@users.noreply.github.com>
2023-10-20 11:06:34 +0100

    Update add.py

--- add.py
+++ add.py
@@ -10,3 +10,15 @@ $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+$name = $_POST['name'];
+$email = $_POST['email'];
+
+$sql = "INSERT INTO user_data (name, email) VALUES ('$name', '$email')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
