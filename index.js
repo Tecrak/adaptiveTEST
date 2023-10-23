c2b16072dd96a192e8a8e4246fa148d708f38878
Tecrak <87039557+Tecrak@users.noreply.github.com>
2023-10-23 20:41:39 +0000

    commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//DONE
+//DONE2
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js";
 
@@ -19,13 +19,32 @@ const form = document.getElementById('form');
 form.addEventListener('submit', async function (event) {
   event.preventDefault();
   const name = document.getElementById('name').value;
+  const surname = document.getElementById('surname').value;
   const email = document.getElementById('email').value;
+  const birth = document.getElementById('birth').value;
+  const HTMLS = document.getElementById('HTMLS').value;
+  const CSSS = document.getElementById('CSSS').value;
+  const JSS = document.getElementById('JSS').value;
+  const PHPS = document.getElementById('PHPS').value;
+  const MySQLS = document.getElementById('MySQLS').value;
+  const WebDesignS = document.getElementById('WebDesignS').value;
+  const CV = document.getElementById('CV').value;
 
   try {
     // Add data to Firestore
     const docRef = await addDoc(collection(db, 'user_data'), {
       name: name,
-      email: email
+      surname: surname,
+      email: email,
+      birth: birth,
+      HTMLS: HTMLS,
+      CSSS: CSSS,
+      JSS: JSS,
+      PHPS: PHPS,
+      MySQLS: MySQLS,
+      WebDesignS: WebDesignS,
+      CV: CV
+
     });
     console.log('Document written with ID: ', docRef.id);
     // Optionally, you can redirect the user to a thank you page or display a success message here
